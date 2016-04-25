5b327b67b8f08579d315ff3acc2124a89f87ffc6
desmots <loic.desmots.pro@free.fr>
2016-04-25 23:10:34 +0200

    first run of my first app

--- app/src/main/java/desmots/myfirstapp/MainActivity.java
+++ app/src/main/java/desmots/myfirstapp/MainActivity.java
@@ -2,6 +2,8 @@ package desmots.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onButtonTap(View v){
+        Toast myToast = Toast.makeText(
+                getApplicationContext(),
+                "hummmmm",
+                Toast.LENGTH_LONG);
+        myToast.show();
     }
 }
